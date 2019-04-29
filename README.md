fab64a4ca466ccbdc350111d42a0b439a626ad1f
Himavanth <contacthima@yahoo.com>
2019-04-29 16:52:31 +0530

    First Commit

---
+++ README.md
@@ -0,0 +1,53 @@
+This module is an OW action to store access tokens in Dynamodb.
+
+## Usage
+This can be used to store access tokens and the corresponding expiry dates.
+The mandatory parameters required for this action to talk to dynamodb include accessKeyId, secretAccessKey, sessionToken (only in case of temporary credentials), region and endpoint.
+Some important terminology is listed below.
+profileID is <IntegrationID>:oauth:<IMS UserID>
+provider is "adobe" for IMS
+accessToken is either the user accessToken or service token as per IMS terminilogy
+Call the action with any of the following combinations for the described functionality.
+
+
+## Storing
+Takes three mandatory parameters and stores the tokens in dynamodb.
+```
+profileID
+provider
+accessToken
+```
+
+## Retrieving
+Takes two mandatory parameters and returns the corresponding accessToken.
+```
+profileID
+provider
+```
+
+## Deleting
+Takes two mandatory parameters and deletes the corresponding accessToken.
+```
+profileID
+provider
+```
+
+## Schema
+The dynamodb tables required for this action should have the following schema
+```
+#Profile Table
+profileID (Partition Key)
+provider (Sort Key)
+
+#Refresh Table
+profileID (Partition Key)
+expiryDate (Sort Key)
+```
+
+### Contributing
+
+Contributions are welcomed! Read the [Contributing Guide](./.github/CONTRIBUTING.md) for more information.
+
+### Licensing
+
+This project is licensed under the Apache V2 License. See [LICENSE](LICENSE) for more information.
---
+++ src/action/dynamodbcache.js
@@ -0,0 +1,224 @@
+/*
+Copyright 2019 Adobe. All rights reserved.
+This file is licensed to you under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License. You may obtain a copy
+of the License at http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software distributed under
+the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+OF ANY KIND, either express or implied. See the License for the specific language
+governing permissions and limitations under the License.
+*/
+var crypto = require('./crypto');
+var AWS = require('aws-sdk');
+
+var mycrypto = new crypto();
+var _resolve = null;
+
+function main(params) {
+  return new Promise((resolve, reject) => {
+    //console.log(params);
+    if (_fail_on_missing(["accessKeyId","secretAccessKey"], params, reject) ) {
+      return;
+    }
+    AWS.config.update({
+      accessKeyId: params.accessKeyId,
+      secretAccessKey: params.secretAccessKey,
+      sessionToken: params.sessionToken,
+      region: params.region || "us-east-2",
+      endpoint: params.endpoint || "https://dynamodb.us-east-2.amazonaws.com"
+    });
+
+    let dynamodb_client = new AWS.DynamoDB.DocumentClient();
+    if (params.accessToken && params.profileID) {
+      //console.log("Setting")
+      _set_handler(params, resolve, reject, dynamodb_client);
+    } else {
+      if(params.profileID){
+        if(params.delete === true){
+          //console.log("Delete key:" + params.profileID);
+          deleteTokens(params, resolve, reject, dynamodb_client);
+        }else{
+          //console.log("GET key:" + params.profileID);
+          _get_handler(params, resolve, reject, dynamodb_client);
+        }
+      } else {
+        resolve({message:"Why are you even here!!!"})
+      }
+    }
+  });
+}
+
+function _set_handler(params, resolve, reject, dynamodb_client) {
+  if (_fail_on_missing(["profileID","provider","accessToken"], params, reject) ) {
+    return;
+  }
+  params.accessToken = mycrypto.encryptString(params.profileID, params.accessToken);
+  params.refreshToken = params.refreshToken
+                            ? mycrypto.encryptString(params.profileID, params.refreshToken)
+                            : "";
+  console.log("Persisting..."+params.profileID);
+
+  addProfile(params, resolve, reject, dynamodb_client)
+}
+
+function _get_handler(params, resolve, reject, dynamodb_client) {
+  if (_fail_on_missing(["profileID"], params, reject) ) return;
+
+  let profileID = params.profileID
+  let provider = params.provider || null;
+  const get_dynamodb_handler = (err, response) => {
+    if(rejectIfError(err, reject, "dynamodb_get")) return
+    if(response.Item){//Return the profile
+      let profile = response.Item
+      profile.accessToken = mycrypto.decryptString(profile.profileID, profile.accessToken)
+      profile.refreshToken = mycrypto.decryptString(profile.profileID, profile.refreshToken)
+      resolve(profile);
+    }else{//Return all the providers of this user
+      let profiles = response.Items || [];
+      var providers = [];
+      for (var i = 0; i < profiles.length; i++) {
+        providers.push(profiles[i].provider);
+      }
+      resolve({providers: providers});
+    }
+  }
+
+  console.log("Reading persisted key info...");
+  if(provider !==null){ //Get one specific identity of this user
+    var dbparams = {
+        TableName: "Profile",
+        Key:{
+            "profileID": profileID,
+            "provider": provider
+        }
+    };
+    dynamodb_client.get(dbparams, get_dynamodb_handler);
+  }else{ //Get all identities of this user
+    var dbparams = {
+        TableName: "Profile",
+        KeyConditionExpression: "profileID = :pID",
+        ExpressionAttributeValues: {
+            ":pID":profileID
+        }
+    };
+    dynamodb_client.query(dbparams, get_dynamodb_handler);
+  }
+
+}
+
+function _get_expiring_on_date(params, resolve, reject, dynamodb_client) {
+  if (_fail_on_missing(["expiryDate"], params, reject) ) return;
+
+  let expiryDate = params.expiryDate;
+  const get_expiring_handler = (err, response) => {
+    if(rejectIfError(err, reject, "dynamodb_get")) return
+    resolve(response);
+  }
+  let dbparams = {
+      TableName: "RefreshToken",
+      KeyConditionExpression: "expiryDate = :expDate",
+      ExpressionAttributeValues: {
+          ":expDate":expiryDate
+      }
+  };
+  dynamodb_client.query(dbparams, get_expiring_handler);
+}
+
+function deleteTokens(params, resolve, reject, dynamodb_client) {
+  let {profileID, provider} = params
+  let dbparams = {
+      TableName :"Profile",
+      Key:{
+        "profileID": profileID,
+        "provider": provider
+      }
+  }
+
+  dynamodb_client.delete(dbparams,
+    function(err, data) {
+      if(rejectIfError(err, reject, "dynamodb_delete_profile")) return
+      let expiryDate = data.expiryDate;
+      dynamodb_client.delete({
+              TableName:"RefreshToken",
+              Key:{"profileID":profileID, "expiryDate":expiryDate}
+            }, function(err, data){
+              if(rejectIfError(err, reject, "dynamodb_delete_refreshtoken")) return
+              resolve({message:"Deleted successfully"})
+            });
+    });
+}
+
+function addProfile(params, resolve, reject, dynamodb_client) {
+  let { profileID, provider, accessToken, accessTokenExpiry, refreshToken, refreshTokenExpiry, context} = params;
+  var dbparams = {
+      TableName :"Profile",
+      Item:{
+          "profileID": profileID,
+          "provider": provider,
+          "accessToken": accessToken,
+          "accessTokenExpiry": accessTokenExpiry,
+          "refreshToken": refreshToken,
+          "refreshTokenExpiry": refreshTokenExpiry
+          }
+    }
+  let set_profile_handler = (err, response) => {
+      if(rejectIfError(err, reject, "dynamodb_put")) return
+      if(provider == "adobe"){
+        addRefreshToken(params, resolve, reject, dynamodb_client)
+      }else{
+        resolve({
+          profileID: profileID,
+          provider: provider,
+          context: context
+        });
+      }
+  }
+  dynamodb_client.put(dbparams, set_profile_handler);
+}
+
+function addRefreshToken(params, resolve, reject, dynamodb_client) {
+  let { profileID, provider, accessToken, accessTokenExpiry, refreshToken, refreshTokenExpiry, context} = params;
+  var refreshParams = {
+    TableName : "RefreshToken",
+    Item:{
+      "profileID": profileID,
+      "expiryDate": refreshTokenExpiry || "NA",
+      "provider": provider,
+      "refreshToken": refreshToken
+    }
+  }
+  let set_refreshToken_handler = (err, response) =>{
+    if(rejectIfError(err, reject, "dynamodb_put_refresh")) return;
+    resolve({
+      profileID: profileID,
+      provider: provider,
+      context: context
+    });
+  };
+  dynamodb_client.put(refreshParams, set_refreshToken_handler);
+}
+
+function rejectIfError(err, reject, type){
+  if (err !== null && typeof(err) !== "undefined") {
+    reject({
+      error: err.toString(),
+      type: type
+    })
+    return true
+  }
+  return false
+}
+
+function _fail_on_missing(param_names, params, reject) {
+  for(var param_name of param_names)
+    if (params[param_name] == null || typeof(params[param_name]) == "undefined") {
+      reject({
+        "message": "Parameter " + param_name + " is required."
+      });
+      return true
+    }
+  return false
+}
+
+export default main;
